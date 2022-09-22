0332e7ede552ace856ef29adda2a7820209e17b2
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-09-22 10:54:58 -0400

    Data Size Range

---
+++ DataTypes/src/DataSizeRange.java
@@ -0,0 +1,28 @@
+public class DataSizeRange {
+    public static void main(String[] args) {
+        System.out.println("Int Min value: " + Integer.MIN_VALUE);
+        System.out.println("Int Max value: " + Integer.MAX_VALUE);
+        System.out.println("Int Bytes: " + Integer.BYTES);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Float Min value: " + Float.MIN_VALUE);
+        System.out.println("Float Max value:" + Float.MAX_VALUE);
+        System.out.println("Float Bytes: " + Float.BYTES);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Double Min value: " + Double.MIN_VALUE);
+        System.out.println("Double Max value: " + Double.MAX_VALUE);
+        System.out.println("Double Bytes: " + Double.BYTES);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Character Min value: " + (int)Character.MIN_VALUE);
+        System.out.println("Character Max value: " + (int)Character.MAX_VALUE);
+        System.out.println("Character Bytes: " + Character.BYTES);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Byte Min value: " + Byte.MIN_VALUE);
+        System.out.println("Byte Max value: " + Byte.MAX_VALUE);
+        System.out.println("Byte Bytes: " + Byte.BYTES);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Short Min Value: " + Short.MIN_VALUE);
+        System.out.println("Short Max Value: " + Short.MAX_VALUE);
+        System.out.println("Short Bytes: " + Short.BYTES);
+        System.out.println("-------------------------------------------------------------");
+    }
+}
