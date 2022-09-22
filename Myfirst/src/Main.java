add747d95788201d3d251d7840698a56fbe3eebc
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-09-21 22:38:51 -0400

    Scanner

--- Myfirst/src/Main.java
+++ Myfirst/src/Main.java
@@ -2,6 +2,8 @@ import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println(args[0]);
     }
 }
