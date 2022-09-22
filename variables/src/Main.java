4c7c7551772baca19f7dbeb0e3f2845b974a27a2
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-09-22 11:23:06 -0400

    Declaring Variables

---
+++ variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 12;
+        int i = 120121;
+        float f = 12.0f;
+        char c ='A';
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+
+    }
+}
