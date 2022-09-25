a863e4ae29395e65aae9686e43555e9c0ad24b37
Hem Parekh <hemparekh@Hems-MacBook-Pro.local>
2022-09-25 15:02:41 -0400

    Operators and Expressions: 1

---
+++ Operator_Expression/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Operators and Expression");
+        System.out.println("-----------------------------------------------------------");
+        int x = 10%3;
+        int y =  10/3;
+        System.out.println("'%' will give us the remainder. For example 10 % 3: " + x);
+        System.out.println("'/' will give us the quotient. For example 10/3: " + y );
+        double z = 12.7 % 3.1;
+        System.out.println(z);
+        int a = 10, b = 5, c = 2;
+        double expression1 =  a + b/2 *c;
+        double expression2 = (a+b)/(2*c);
+        System.out.println(expression1);
+        System.out.println(expression2);
+
+
+    }
+}
