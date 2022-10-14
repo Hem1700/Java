8e1793cd93cb15d4ce0f0e3bee80b449d5d08e16
Hem1700 <hemparekh1596@gmail.com>
2022-10-14 19:55:25 -0400

    Increment Decrement Operators

---
+++ Increment_Decrement/main.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+public class main {
+    public static void main(String args[]){
+        int x = 5 , y , z;
+        y = x++;
+        z = ++x;
+        System.out.println("Value of x: " +x);
+        System.out.println("Value of y: " +y);
+        System.out.println("Value of z: " +z);
+
+
+    }
+}
