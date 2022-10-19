5200ad32a2d5de5f5fcfb56d55e53cab27ff151e
Hem1700 <hemparekh1596@gmail.com>
2022-10-19 16:05:12 -0400

    Widening and Narrowing

---
+++ Widening_Narrowing/main.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.sql.ShardingKey;
+
+public class main {
+
+    public static void main (String args []){
+
+        /* Widening (Upcasting) : Storing the data of a smaller datatype into a larger datatype. 
+        For example storing byte value in short or storing short value in int. This has happens implicitly.
+        The source and destination data type should be compatible. 
+        The size of source data should be less than or equal to destination data.
+        */ 
+        /* When either of the above condition is not matched we have to explicity convert them. 
+         This is called Narrowing (Downcasting).
+        */
+
+        byte b = 10;
+        short s = 10;
+        int i = 10;
+        long l = 10;
+        float f =10;
+        double d = 10;
+        char c = 10;
+        boolean bl = true;
+
+        
+
+        
+
+    }
+    
+}
+ 
