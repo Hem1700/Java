3aa1386af05b6502554737f0b40af5223853c42b
Hem1700 <hemparekh1596@gmail.com>
2022-10-21 14:22:54 -0400

    Formatted Output

---
+++ Formatted_output/main.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class main {
+    
+    public static void main (String args []){
+
+        int x = 10;
+        float y = 10.2f;
+        char z = 'A';
+
+        System.out.printf("Hello\n");
+        System.out.printf("The value of x,y,z is %d %2.1f %c  \n", x,y,z  );
+
+
+    }
+
+}
