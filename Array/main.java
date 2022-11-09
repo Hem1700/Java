16a631b83f78f114cf9e6d969e4faa64c175f414
Hem1700 <hemparekh1596@gmail.com>
2022-11-09 16:14:26 -0500

    Arrays

---
+++ Array/main.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+public class main {
+
+  
+    public static void main (String args []){
+
+    /* Arrays */
+    int A[] = new int[5]; // creation of array
+    System.out.println(A.length);
+    int B[] = {2,4,6,8,10};
+    for(int i = 0; i<5;i++){
+        System.out.println(B[i]); 
+    }
+
+    for (int j = B.length-1; j>=0; j--){
+        System.out.println(B[j]);
+    }
+    for(int x: B){
+        System.out.println(x);
+    }
+    }
+
+    
+}
