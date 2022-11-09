c75d1a5b0ded1314c10301f5fcb5e1eefb5d4ec4
Hem1700 <hemparekh1596@gmail.com>
2022-11-09 16:16:47 -0500

    Array

--- Array/main.java
+++ Array/main.java
@@ -7,9 +7,14 @@ public class main {
     public static void main (String args []){
 
     /* Arrays */
+    // Initializing Array methods
     int A[] = new int[5]; // creation of array
     System.out.println(A.length);
     int B[] = {2,4,6,8,10};
+    int []C = new int [5];
+    int[] D = new int[10];
+
+    // Accessing elements of array
     for(int i = 0; i<5;i++){
         System.out.println(B[i]); 
     }
