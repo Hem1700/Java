add998f4b1c4b724258085a092c792e3764f79ee
Hem1700 <hemparekh1596@gmail.com>
2022-11-12 17:08:00 -0500

    2D Array:2

--- 2D_Array/main.java
+++ 2D_Array/main.java
@@ -12,19 +12,23 @@ public class main {
         int B[][] = {{1,2,3,4}, {2,4,6,8}, {3,5,7,9}};
         int C[][];
         C = new int[3][4];
-
+        int []E[] = new int[5][5];
+        int[] F, G[];
+        // F is single dimensional array and G is 2D Array
+ 
         //Access the array
+        System.out.println("B Array\n");
         for (int i =0; i<B.length; i++){
             for (int j =0; j<B[0].length; j++){
-                System.out.println(B[i][j]+ " ");
+                System.out.print(B[i][j]+ " ");
             }
             System.out.println("\n");
         }
-
-        // For each looo
+        System.out.println("A Array\n");
+        // For each loop
         for(int x[]:A){
             for(int y: x){
-                System.out.println(y+ " ");
+                System.out.print(y+ " ");
             }
             System.out.println("\n");
         }
@@ -35,7 +39,15 @@ public class main {
         D[0] = new int[2];
         D[1] = new int[4];
         D[2] = new int[3];
-        
+
+        System.out.println("D Array\n");
+        for (int i = 0; i<D.length; i++){
+            for (int j = 0 ; j<D[i].length; j++){
+                System.out.print(D[i][j]+ " ");
+            }
+            System.out.println("\n");
+        }
+
 
 
     }
