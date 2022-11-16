4148a87a80d22808b153ceda3b8b752558bd5865
Hem1700 <hemparekh1596@gmail.com>
2022-11-16 10:56:42 -0500

    Variable Argument:2

--- variable_argument/main.java
+++ variable_argument/main.java
@@ -6,17 +6,27 @@ public class main {
     static void show (int... x)    // int []x
     {
         for (int a:x){
-            System.out.println(a);
+            System.out.print(a + " " );
         }
+        System.out.println();
 
     }
 
-    public static void main (String args []){
+    static void showList(String ...S){
+        for(int i = 0; i < S.length; i++){
+            System.out.println(i+1 + ". " + S[i]);
+        }
+    }
+
+    public static void main (String args [])
+    {
         System.out.println("Variable Argument");
         show();
         show(10,20,30);
         show(10,20,30,40,50);
         show(new int[]{10,30,50,70});
+
+        showList("John" , "Smith", "Ajay" , "Walter");
     }
     
 }
