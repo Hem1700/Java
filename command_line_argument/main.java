7c71f9c17230ef597bbb9cd3231576b7d1590a34
Hem1700 <hemparekh1596@gmail.com>
2022-11-19 00:07:14 -0500

    Command Line Argument

---
+++ command_line_argument/main.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+public class main {
+
+    public static void main (String args[]){
+        System.out.println("Command Line Arguments");
+        for(String s: args){
+            System.out.println(s);
+        }
+    }
+    
+}
