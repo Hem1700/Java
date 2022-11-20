8948cf19d9bf4ca8cd4adc0b5f9aed92812073d3
Hem1700 <hemparekh1596@gmail.com>
2022-11-20 11:55:30 -0500

    Object Oriented Programming

--- Challanges/cmd_argument.java
+++ Challanges/cmd_argument.java
@@ -7,11 +7,14 @@ public class cmd_argument {
 
     public static void main (String args[]){
         int sum = 0;
-        System.out.println(args[0]);
+        if(args[0].equals("add")){
             for (int i = 0; i<args.length-1; i++){
                 sum = sum + Integer.parseInt(args[i+1]);     
         }
         System.out.println(sum);
+        }
+        
+            
           
     }
     
---
+++ oops/main.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.*;
+
+class Test{
+    public int value;
+}
+
+
+
+public class main {
+
+    public static void main (String args[]){
+        System.out.println("Object Oriented Programming");
+        /* Principles of OOPs
+         * 1. Abstraction : Hiding Internal details and showing only required features.
+         * 2. Encapsulation
+         * 3. Inheritance
+         * 4. Polymorphism
+         */
+        
+        
+        
+        
+    }
+    
+}
